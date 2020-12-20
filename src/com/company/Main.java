4238baf00184a54ed33473551f07aefd39de18c2
SudharHP <haripriyar9798@gmail.com>
2020-12-20 09:23:55 -0500

    First commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+      System.out.println("This is my first java project in linux");
+      System.out.println("This is my first java project in GIT");
+
+    }
+}
